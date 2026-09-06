3cb84c45489f1f7586f1e4be2c5cecb7c9c8f07d
jakus2323-hue <jakus2323@gmail.com>
2026-09-06 17:52:03 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = "digger-loads-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      });
+    }).catch(() => caches.match("./index.html"))
+  );
+});
